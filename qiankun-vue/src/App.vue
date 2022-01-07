acc79de3676318daad12bd2da5ea986e3ab38ba9
商华伟 <xiahouwei@aliyun.com>
2022-01-07 19:03:19 +0800

    init

---
+++ qiankun-vue/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name:'名字'
+    }
+  },
+}
+</script>
+
+<style  scoped>
+/* div,a {
+  color: red;
+  font-size: 32px;
+} */
+</style>
